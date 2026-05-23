c4c6d6fbf7aef6f03df0e63f460c2c6ee7ab042a
Echo <ravensandechos@gmail.com>
2026-05-23 19:24:50 +0100

    2 signals

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include "rtos_lite.h"
 
 void blink25();
+void blink1();
 
 int main()
 {
   gpio_init(25);
   gpio_set_dir(25, GPIO_OUT);
+  gpio_init(1);
+  gpio_set_dir(1, GPIO_OUT);
 
   OS_LITE::OS_TASKS = {{
     {blink25, 1, OS_LITE::TaskState::READY, 0, {}},
+    {blink1, 2, OS_LITE::TaskState::READY, 0, {}},
   }};
 
-  OS_LITE::task_count = 1;
+  OS_LITE::task_count = 2;
   OS_LITE::scheduler_init();
 
   return 0;
@@ -24,4 +28,13 @@ void blink25()
   state = !state;
 
   OS_LITE::task_sleep(&OS_LITE::OS_TASKS[0], 2000); // 500 ms
+}
+
+void blink1()
+{
+  static bool state{false};
+  gpio_put(1, state);
+  state = !state;
+
+  OS_LITE::task_sleep(&OS_LITE::OS_TASKS[1], 1000); // 500 ms
 }
